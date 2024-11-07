bc058fd0931e7436c4c7236070efb79148f06dd4
mzelesky <mzelesky@princeton.edu>
2024-11-07 16:25:24 -0500

    add script to remove NjP prefix from 773/774 (#14)

--- lib/lsp-data.rb
+++ lib/lsp-data.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'marc'
+require 'nokogiri'
+require 'marc_cleanup'
 require_relative 'lsp-data/connection'
 require_relative 'lsp-data/match_key_overlap'
 require_relative 'lsp-data/standard_nums'
