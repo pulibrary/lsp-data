7a20af1889f9d3d0e8740d5a723dbdfd6e6219ef
Mark Zelesky <mzelesky@princeton.edu>
2024-11-22 10:54:57 -0500

    retrieve call numbers from holdings

--- lib/lsp-data/marc_data.rb
+++ lib/lsp-data/marc_data.rb
@@ -2,12 +2,30 @@
 
 module LspData
   def call_num_from_bib_field(record:, field_tag:)
-    return nil unless record[field_tag]
+    return [] unless record[field_tag]
 
     call_nums = []
     record.fields(field_tag).each do |field|
       call_num = field.subfields.select do |subfield|
         %w[a b].include?(subfield.code)
+      end.map(&:value).join(' ')
+      call_num.strip!
+      call_nums << call_num
+    end
+    call_nums
+  end
+
+  def call_num_from_alma_holding_field(record:, field_tag:, inst_suffix:, lc_only: true)
+    return [] unless record[field_tag]
+
+    call_nums = []
+    record.fields(field_tag).select do |field|
+      field['8'] =~ /22[0-9]+#{inst_suffix}$/
+    end.each do |field|
+      next if lc_only && field.indicator1 != '0'
+
+      call_num = field.subfields.select do |subfield|
+        %w[h i].include?(subfield.code)
       end.map(&:value).join(' ')
       call_num.strip!
       call_nums << call_num
--- spec/marc_data_spec.rb
+++ spec/marc_data_spec.rb
@@ -25,11 +25,11 @@ RSpec.describe 'call_num_from_bib_field' do
   context 'record has 090 field and 050 field and 090 is target field' do
     let(:fields) do
       [
-        { '050' => { 'indicator1' => ' ',
-                     'indicator2' => '0',
+        { '050' => { 'ind1' => ' ',
+                     'ind2' => '0',
                      'subfields' => [{ 'a' => 'M269' }, { 'b' => '.C69 ' }] } },
-        { '090' => { 'indicator1' => ' ',
-                     'indicator2' => '4',
+        { '090' => { 'ind1' => ' ',
+                     'ind2' => '4',
                      'subfields' => [{ 'a' => 'M3.1' }, { 'b' => '.C69 2012' }] } }
       ]
     end
@@ -37,6 +37,89 @@ RSpec.describe 'call_num_from_bib_field' do
       target_array = ['M3.1 .C69 2012']
       expect(LspData.call_num_from_bib_field(record: record,
                                        field_tag: '090')).to eq target_array
+    end
+  end
+end
+
+RSpec.describe 'call_num_from_alma_holding_field' do
+  let(:leader) { '01104naa a2200289 i 4500' }
+  let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+  let(:inst_suffix) { '6124' }
+  let(:field_tag) { '852' }
+
+  context 'record has 852 with no holding id' do
+    let(:fields) do
+      [
+        { '852' => { 'ind1' => '0',
+                     'ind2' => ' ',
+                     'subfields' => [{ 'h' => 'bib 852' }, { 'i' => '.C69 ' }] } }
+      ]
+    end
+    it 'returns no call numbers' do
+      expect(LspData.call_num_from_alma_holding_field(record: record,
+                                                      field_tag: field_tag,
+                                                      inst_suffix: inst_suffix)).to be_empty
+    end
+  end
+
+  context 'record has 852 with wrong institution suffix' do
+    let(:fields) do
+      [
+        { '852' => { 'ind1' => '0',
+                     'ind2' => ' ',
+                     'subfields' => [
+                       { 'h' => 'M269' },
+                       { 'i' => '.C69 ' },
+                       { '8' => '2213124' }
+                     ] } }
+      ]
+    end
+    it 'returns no call numbers' do
+      expect(LspData.call_num_from_alma_holding_field(record: record,
+                                                      field_tag: field_tag,
+                                                      inst_suffix: inst_suffix)).to be_empty
+    end
+  end
+
+  context 'record has one 852 with institution suffix and one without' do
+    let(:fields) do
+      [
+        { '852' => { 'ind1' => ' ',
+                     'ind2' => ' ',
+                     'subfields' => [
+                       { 'h' => 'M269' },
+                       { 'i' => '.C69 ' },
+                       { '8' => '2216124' }
+                     ] } },
+        { '852' => { 'ind1' => '0',
+                     'ind2' => ' ',
+                     'subfields' => [
+                       { 'h' => 'M3.1' },
+                       { 'i' => '.C69 ' }
+                     ] } }
+      ]
+    end
+    it 'returns correct call numbers' do
+      target_array = ['M269 .C69']
+      expect(LspData.call_num_from_alma_holding_field(record: record,
+                                                      field_tag: field_tag,
+                                                      inst_suffix: inst_suffix,
+                                                      lc_only: false)).to eq target_array
+    end
+  end
+
+  context 'record has no 852 fields' do
+    let(:fields) do
+      [
+        { '050' => { 'ind1' => ' ',
+                     'ind2' => '0',
+                     'subfields' => [{ 'a' => 'M269' }, { 'b' => '.C69 ' }] } },
+      ]
+    end
+    it 'returns no call numbers' do
+      expect(LspData.call_num_from_alma_holding_field(record: record,
+                                                      field_tag: field_tag,
+                                                      inst_suffix: inst_suffix)).to be_empty
     end
   end
 end
