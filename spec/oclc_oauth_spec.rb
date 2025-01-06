c834ca3ed5557e4bb6e01b15e1a89f3794c7845a
Mark Zelesky <mzelesky@princeton.edu>
2025-01-06 16:49:26 -0500

    address timezone issue

--- spec/oclc_oauth_spec.rb
+++ spec/oclc_oauth_spec.rb
@@ -2,13 +2,14 @@
 
 require_relative './../lib/lsp-data'
 require 'spec_helper'
-require 'byebug'
+
 RSpec.describe LspData::OCLCOAuth do
   subject(:oauth) do
     described_class.new(client_id: client_id, client_secret: client_secret, url: url, scope: scope)
   end
 
   let(:url) { 'https://oauth.oclc.org/token' }
+
   context 'Connection information is provided' do
     let(:client_id) { 'id' }
     let(:client_secret) { 'secret' }
@@ -17,7 +18,7 @@ RSpec.describe LspData::OCLCOAuth do
     it 'returns authorization information' do
       stub_oauth(fixture: 'oclc_oauth_response.json', url: url, scope: scope)
       expect(oauth.response[:status]).to eq 200
-      expect(oauth.response[:expiration]).to eq Time.new(2025, 1, 6, 10, 28, 30)
+      expect(oauth.response[:expiration].utc).to eq Time.utc(2025, 1, 6, 15, 28, 30)
       expect(oauth.response[:token]).to eq 'token'
     end
   end
