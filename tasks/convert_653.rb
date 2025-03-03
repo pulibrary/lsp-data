61a2666e9513d5bd631f2d0d2c741ad261fefa73
tventimi <tventimi@princeton.edu>
2025-03-03 14:25:34 -0500

    use replace_fields, generate report on 653 field counts
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

--- tasks/convert_653.rb
+++ tasks/convert_653.rb
@@ -14,9 +14,10 @@ input_dir = ENV['DATA_INPUT_DIR']
 output_dir = ENV['DATA_OUTPUT_DIR']
 
 replacements_file = "#{input_dir}/653_replacements.csv"
-replacements = Hash.new
+replacements = []
 CSV.foreach(replacements_file, headers: true) do |row|
-  source_string = row[0]
+  source_string = row[0].strip
+  source_field = MARC::DataField.new('653', ' ', ' ', ['a',source_string])
   replacement_string = row[1]
   r_tag = replacement_string[0..2]
   r_ind1 = replacement_string[4]
@@ -29,28 +30,46 @@ CSV.foreach(replacements_file, headers: true) do |row|
       sfcontent = sf[1..].strip
       replacement_field.append(MARC::Subfield.new(sfcode,sfcontent))
   end
-  replacements[source_string] = replacement_field
+  replacements << {source_field: source_field, replacement_field: replacement_field, ignore_indicators: true}
 end
 
 marc_file = "#{input_dir}/653_original.marcxml"
 marc_reader = MARC::XMLReader.new(marc_file, parser: 'magic', ignore_namespace: true)
 marc_writer = MARC::XMLWriter.new("#{output_dir}/653_replaced.marcxml")
 marc_reader.each do |record|
-  record_changed = false
-  record.fields('653').each do |f|
-    if f.subfields.count == 1
-      f653_content = f['a']
-      if replacements.key?(f653_content)    
-        source_field = MARC::DataField.new('653', ' ', ' ', ['a',f653_content])
-        record = MarcCleanup.replace_field(source_field: source_field,
-                                           replacement_field: replacements[f653_content],
-                                           record: record,
-                                           ignore_indicators: true)
-        record_changed = true
-      end
-    end
-  end 
-  marc_writer.write(record) if record_changed
+  count_653_before = record.fields('653').size
+  record = MarcCleanup.replace_fields(field_array: replacements, record: record)
+  marc_writer.write(record) if count_653_before != record.fields('653').size
 end
 marc_writer.close
-             
+
+term_hash = {}
+changed_mmsids = []
+marc_reader = MARC::XMLReader.new("#{output_dir}/653_replaced.marcxml", parser: 'magic', ignore_namespace: true)
+marc_reader.each do |record|
+  changed_mmsids << record["001"].value
+  record.fields('653').each do |f|
+    term = f['a'].strip
+    term_hash[term] ||= { before_count: 0, after_count: 0 }
+    term_hash[term][:after_count] += 1
+  end
+end 
+
+marc_reader = MARC::XMLReader.new("#{output_dir}/653_original.marcxml", parser: 'magic', ignore_namespace: true)
+marc_reader.each do |record|
+  record.fields('653').each do |f|
+    term = f['a'].strip
+    term_hash[term] ||= { before_count: 0, after_count: 0 }
+    term_hash[term][:before_count] += 1
+    if !changed_mmsids.include?(record["001"].value)
+      term_hash[term][:after_count] += 1
+    end
+  end
+end
+
+File.open("#{output_dir}/653_report.tsv", 'w') do |output|
+  output.puts("Term\tBefore count\tAfter count")
+  term_hash.each do |term, counts|
+    output.puts("#{term}\t#{counts[:before_count].to_s}\t#{counts[:after_count].to_s}")
+  end
+end
