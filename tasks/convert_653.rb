bee8dfcbdbd6495bf4eaf3e2db650651f48c375e
tventimi <tventimi@princeton.edu>
2025-03-03 10:13:17 -0500

    update marc_cleanup to 0.9.5 draft of 653 conversion script

--- tasks/convert_653.rb
+++ tasks/convert_653.rb
@@ -7,25 +7,50 @@
 ###   a hash of source values and target values
 
 require_relative './../lib/lsp-data'
+require 'CSV'
+require 'byebug'
+
 input_dir = ENV['DATA_INPUT_DIR']
 output_dir = ENV['DATA_OUTPUT_DIR']
-writer = MARC::XMLWriter.new("#{output_dir}/653_replaced.marcxml")
-replacements = [
-                 { source: "653    $a Activity books",
-                   target: MARC::DataField.new('655',' ','7',['a','Activity books.'],['2','lcgft'])  
-                 }
-               ]
 
-file = "#{input_dir}/653_original.marcxml"
-reader = MARC::XMLReader.new(file, parser: 'magic', ignore_namespace: true)
-reader.each do |record|
-  replacements.each do |rep|
-    record = MarcCleanup.replace_field(field_string: rep[:source], 
-                                       replacement_field: rep[:target], 
-                                       record: record)
-    end
-    writer.write(record)
+replacements_file = "#{input_dir}/653_replacements.csv"
+replacements = Hash.new
+CSV.foreach(replacements_file, headers: true) do |row|
+  source_string = row[0]
+  replacement_string = row[1]
+  r_tag = replacement_string[0..2]
+  r_ind1 = replacement_string[4]
+  r_ind2 = replacement_string[5]
+  r_content = replacement_string[7..]
+  replacement_field = MARC::DataField.new(r_tag, r_ind1, r_ind2)
+  r_content.split('$').each do |sf|
+      next if sf == ""
+      sfcode = sf[0]
+      sfcontent = sf[1..].strip
+      replacement_field.append(MARC::Subfield.new(sfcode,sfcontent))
+  end
+  replacements[source_string] = replacement_field
 end
-writer.close
 
+marc_file = "#{input_dir}/653_original.marcxml"
+marc_reader = MARC::XMLReader.new(marc_file, parser: 'magic', ignore_namespace: true)
+marc_writer = MARC::XMLWriter.new("#{output_dir}/653_replaced.marcxml")
+marc_reader.each do |record|
+  record_changed = false
+  record.fields('653').each do |f|
+    if f.subfields.count == 1
+      f653_content = f['a']
+      if replacements.key?(f653_content)    
+        source_field = MARC::DataField.new('653', ' ', ' ', ['a',f653_content])
+        record = MarcCleanup.replace_field(source_field: source_field,
+                                           replacement_field: replacements[f653_content],
+                                           record: record,
+                                           ignore_indicators: true)
+        record_changed = true
+      end
+    end
+  end 
+  marc_writer.write(record) if record_changed
+end
+marc_writer.close
              
