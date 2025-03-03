3e8dad86b298b45c160aa0a54cea6e1793e028b7
tventimi <tventimi@princeton.edu>
2025-03-03 15:12:38 -0500

    rubocop adjustments

--- tasks/convert_653.rb
+++ tasks/convert_653.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-### Acceptance criteria:
-### - The 653 field of Activity books found in 9963637633506421 is replaced with the target 
-###   field of `655 _7 $a Activity books. $2 lcgft
-### - A mapping mechanism is developed in the script to translate lines of the spreadsheet into 
-###   a hash of source values and target values
-
 require_relative './../lib/lsp-data'
 require 'CSV'
 require 'byebug'
@@ -17,7 +11,7 @@ replacements_file = "#{input_dir}/653_replacements.csv"
 replacements = []
 CSV.foreach(replacements_file, headers: true) do |row|
   source_string = row[0].strip
-  source_field = MARC::DataField.new('653', ' ', ' ', ['a',source_string])
+  source_field = MARC::DataField.new('653', ' ', ' ', ['a', source_string])
   replacement_string = row[1]
   r_tag = replacement_string[0..2]
   r_ind1 = replacement_string[4]
@@ -25,12 +19,13 @@ CSV.foreach(replacements_file, headers: true) do |row|
   r_content = replacement_string[7..]
   replacement_field = MARC::DataField.new(r_tag, r_ind1, r_ind2)
   r_content.split('$').each do |sf|
-      next if sf == ""
-      sfcode = sf[0]
-      sfcontent = sf[1..].strip
-      replacement_field.append(MARC::Subfield.new(sfcode,sfcontent))
+    next if sf == ''
+
+    sfcode = sf[0]
+    sfcontent = sf[1..].strip
+    replacement_field.append(MARC::Subfield.new(sfcode, sfcontent))
   end
-  replacements << {source_field: source_field, replacement_field: replacement_field, ignore_indicators: true}
+  replacements << { source_field: source_field, replacement_field: replacement_field, ignore_indicators: true }
 end
 
 marc_file = "#{input_dir}/653_original.marcxml"
@@ -47,13 +42,13 @@ term_hash = {}
 changed_mmsids = []
 marc_reader = MARC::XMLReader.new("#{output_dir}/653_replaced.marcxml", parser: 'magic', ignore_namespace: true)
 marc_reader.each do |record|
-  changed_mmsids << record["001"].value
+  changed_mmsids << record['001'].value
   record.fields('653').each do |f|
     term = f['a'].strip
     term_hash[term] ||= { before_count: 0, after_count: 0 }
     term_hash[term][:after_count] += 1
   end
-end 
+end
 
 marc_reader = MARC::XMLReader.new("#{output_dir}/653_original.marcxml", parser: 'magic', ignore_namespace: true)
 marc_reader.each do |record|
@@ -61,15 +56,13 @@ marc_reader.each do |record|
     term = f['a'].strip
     term_hash[term] ||= { before_count: 0, after_count: 0 }
     term_hash[term][:before_count] += 1
-    if !changed_mmsids.include?(record["001"].value)
-      term_hash[term][:after_count] += 1
-    end
+    term_hash[term][:after_count] += 1 unless changed_mmsids.include?(record['001'].value)
   end
 end
 
 File.open("#{output_dir}/653_report.tsv", 'w') do |output|
   output.puts("Term\tBefore count\tAfter count")
   term_hash.each do |term, counts|
-    output.puts("#{term}\t#{counts[:before_count].to_s}\t#{counts[:after_count].to_s}")
+    output.puts("#{term}\t#{counts[:before_count]}\t#{counts[:after_count]}")
   end
 end
