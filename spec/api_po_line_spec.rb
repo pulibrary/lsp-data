5e6c77c1eb9c9f5b7c7e89989a7eeb9a0fa03afa
Paul Diskin <pdiskin@princeton.edu>
2025-03-14 15:02:28 -0400

    created ApiPoLine and made a successful test
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

--- spec/api_po_line_spec.rb
+++ spec/api_po_line_spec.rb
@@ -4,7 +4,7 @@ require_relative './../lib/lsp-data'
 require 'spec_helper'
 RSpec.describe LspData::ApiPoLine do
   subject(:po_line) do
-    described_class.new(po_line: stub_json_fixture(fixture: fixture))
+    described_class.new(po_line_json: stub_json_fixture(fixture: fixture))
   end
   context 'PO Line retrieved via API' do
     let(:fixture) { 'po_line.json' }
