4c4700cf0bb18348e71366a0abe5b43cef0b7fed
Paul Diskin <pdiskin@princeton.edu>
2025-03-14 11:04:20 -0400

    added poline fixture and test
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

---
+++ spec/api_po_line_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative './../lib/lsp-data'
+require 'spec_helper'
+RSpec.describe LspData::ApiPoLine do
+  subject(:po_line) do
+    described_class.new(po_line: stub_json_fixture(fixture: fixture))
+  end
+  context 'PO Line retrieved via API' do
+    let(:fixture) { 'po_line.json' }
+    it 'returns the po-line number' do
+      expect(po_line.number).to eq 'POL-1'
+    end
+  end
+end
