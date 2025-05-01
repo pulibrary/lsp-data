bb651124d96adaf9d11be73eff1f6515f7393981
Mark Zelesky <mzelesky@princeton.edu>
2025-05-01 09:19:00 -0400

    add logic to handle retirees and inactive patrons

--- spec/oit_person_to_alma_spec.rb
+++ spec/oit_person_to_alma_spec.rb
@@ -22,4 +22,34 @@ RSpec.describe LspData::OitPersonToAlma do
       expect(builder.to_xml).to eq alma_xml.to_xml
     end
   end
+
+  context 'Person is a retired faculty member' do
+    let(:oit_fixture) { 'oit_person_retiree.json' }
+    let(:alma_fixture) { 'alma_person_retiree.xml' }
+    let(:person) { stub_json_fixture(fixture: oit_fixture) }
+    let(:alma_xml) { stub_xml_fixture(fixture: alma_fixture) }
+    it 'transforms the person into an inactive Alma person' do
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.users do
+          OitPersonToAlma.new(person: person, xml: xml).convert
+        end
+      end
+      expect(builder.to_xml).to eq alma_xml.to_xml
+    end
+  end
+
+  context 'Person is an inactive graduate student' do
+    let(:oit_fixture) { 'oit_person_grad_ineligible.json' }
+    let(:alma_fixture) { 'alma_person_grad_ineligible.xml' }
+    let(:person) { stub_json_fixture(fixture: oit_fixture) }
+    let(:alma_xml) { stub_xml_fixture(fixture: alma_fixture) }
+    it 'transforms the person into an inactive Alma person' do
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.users do
+          OitPersonToAlma.new(person: person, xml: xml).convert
+        end
+      end
+      expect(builder.to_xml).to eq alma_xml.to_xml
+    end
+  end
 end
