61f23fc23600ad04d8b62b899919f53c0e5b67ff
Mark Zelesky <mzelesky@princeton.edu>
2025-05-06 09:50:02 -0400

    add test for standalone portfolio retrieval

--- spec/api_retrieve_portfolio_spec.rb
+++ spec/api_retrieve_portfolio_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe LspData::ApiRetrievePortfolio do
   let(:url) { 'https://api-na.exlibrisgroup.com' }
   let(:conn) { LspData.api_conn(url) }
   let(:api_key) { 'apikey' }
+
   context 'Portfolio in an electronic collection' do
     let(:mms_id) { '99101234' }
     let(:portfolio_id) { '53101234' }
@@ -22,6 +23,18 @@ RSpec.describe LspData::ApiRetrievePortfolio do
       stub_get_portfolio_response(mms_id: mms_id, portfolio_id: portfolio_id, fixture: fixture)
       expect(retrieval.response[:body]['id']).to eq portfolio_id
       expect(retrieval.response[:body]['electronic_collection']['id']['value']).to eq '61101234'
+    end
+  end
+
+  context 'Portfolio is a standalone portfolio' do
+    let(:mms_id) { '99101234' }
+    let(:portfolio_id) { '53101234' }
+    let(:fixture) { 'standalone_portfolio.json' }
+    it 'returns portfolio information with no collection information' do
+      stub_get_portfolio_response(mms_id: mms_id, portfolio_id: portfolio_id, fixture: fixture)
+      expect(retrieval.response[:body]['id']).to eq portfolio_id
+      expect(retrieval.response[:body]['electronic_collection']['id']['value']).to eq ''
+      expect(retrieval.response[:body]['is_standalone']).to be true
     end
   end
 end
