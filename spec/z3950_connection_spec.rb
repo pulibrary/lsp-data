fa2ea318b04993e42ef119a1a79da190f8918609
tventimi <tventimi@princeton.edu>
2025-07-24 17:49:14 -0400

    Add Z39.50 search capability (#107)
    * Add Z39.50 search capability
    
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>
    
    * implement Z39.50 proposal
    
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/z3950_connection_spec.rb
+++ spec/z3950_connection_spec.rb
@@ -8,14 +8,26 @@ RSpec.describe LspData::Z3950Connection do
     described_class.new(host: host, port: port, database_name: database_name, element_set_name: element_set_name)
   end
 
-  context 'valid connection parameters' do
-    let(:host) { 'lx2.loc.gov' }
-    let(:port) { 210 }
-    let(:database_name) { 'LCDB' }
-    let(:element_set_name) { 'F' }
+  let(:host) { 'lx2.loc.gov' }
+  let(:port) { 210 }
+  let(:database_name) { 'LCDB' }
+  let(:element_set_name) { 'F' }
 
+  context 'valid connection parameters' do
     it 'returns a ZOOM::Connection class' do
       expect(connection.connection.class).to eq ZOOM::Connection
+    end
+  end
+
+  context 'ISBN provided that exists in database for a search' do
+    let(:index) { 7 }
+    let(:identifier) { '9781984899422' }
+
+    it 'returns a valid record' do
+      record = connection.search(index: index, identifier: identifier).first
+      f020 = record.fields('020').map { |field| field['a'] }
+      expect(record.class).to eq MARC::Record
+      expect(f020).to include identifier
     end
   end
 end
