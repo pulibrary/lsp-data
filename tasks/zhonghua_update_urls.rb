c73a75b93899aa1e55a7b3aeb70343524896a2f8
Mark Zelesky <mzelesky@princeton.edu>
2025-10-23 15:43:57 -0400

    update zhonghua_update_urls process for new changes

--- tasks/zhonghua_update_urls.rb
+++ tasks/zhonghua_update_urls.rb
@@ -13,33 +13,26 @@ output_dir = ENV['DATA_OUTPUT_DIR']
 ###   make ['linking_details']['url_type_override']['value'] equal to '';
 ###   add the POL
 def modify_portfolio(portfolio:, url:)
-  portfolio['linking_details']['static_url'] = "jkey=#{url}"
+  portfolio['linking_details']['static_url'] = url
   portfolio['linking_details']['url_type_override']['value'] = ''
-  portfolio['linking_details']['dynamic_url'] = ''
-  portfolio['linking_details']['dynamic_url_override'] = ''
   portfolio['linking_details']['static_url_override'] = ''
-  portfolio['linking_details']['url'] = "jkey=#{url}"
+  portfolio['linking_details']['url'] = url
   portfolio['linking_details']['url_type']['value'] = 'static'
-  portfolio['po_line']['value'] = ENV['ZHONGHUA_POL']
   portfolio
 end
 
 ### Step 1: Load in the portfolio IDs and URLs from a report
-object_to_url = {}
+portfolio_to_url = {}
 bib_portfolios = {}
-csv = CSV.open("#{input_dir}/Zhonghua URL mapping.csv", 'r', headers: true, encoding: 'bom|utf-8')
+csv = CSV.open("#{input_dir}/ZhonghuaPortfoliosList_Oct2025.csv", 'r', headers: true, encoding: 'bom|utf-8')
 csv.each do |row|
-  object = row['OLD URL ID']
+  portfolio_id = row['Portfolio Id']
   url = row['New URL']
-  mms_id = row['MMS ID'].to_s
-  next if mms_id == 'FALSE'
-
-  portfolios = row['Portfolio List'].split('¦')
+  mms_id = row['MMS Id']
   bib_portfolios[mms_id] ||= []
-  bib_portfolios[mms_id] += portfolios
-  object_to_url[object] = url
+  bib_portfolios[mms_id] << portfolio_id
+  portfolio_to_url[portfolio_id] = url
 end
-bib_portfolios.each_value(&:uniq!)
 
 ### Step 2: Iterate through all portfolio IDs; if the object ID appears in
 ###   a normalized version of the URL anywhere in the portfolio, update the
@@ -48,7 +41,7 @@ bib_portfolios.each_value(&:uniq!)
 ### Retrieve each portfolio
 url = 'https://api-na.hosted.exlibrisgroup.com'
 conn = LspData.api_conn(url)
-api_key = ENV['ALMA_SANDBOX_BIB_API_KEY']
+api_key = ENV['ALMA_PROD_BIB_API_KEY']
 
 get_responses = {} # hash of portfolio ID to response
 bib_portfolios.each do |mms_id, portfolio_ids|
@@ -61,9 +54,7 @@ bib_portfolios.each do |mms_id, portfolio_ids|
 end
 
 ### Update the portfolios that were successfully retrieved
-object_to_portfolios = {}
 update_responses = {}
-portfolio_to_old_url = {}
 bib_portfolios.each do |mms_id, portfolio_ids|
   portfolio_ids.each do |portfolio_id|
     next if update_responses[portfolio_id]
@@ -71,16 +62,9 @@ bib_portfolios.each do |mms_id, portfolio_ids|
     get_response = get_responses[portfolio_id]
     next unless get_response[:status] == 200
 
-    portfolio = get_response[:body]
-    static_url = portfolio['linking_details']['static_url']
-    portfolio_to_old_url[portfolio_id] = static_url
-    object_id = static_url.gsub(/^.*docbookid=([^&]+)%.*$/, '\1').upcase
-    url = object_to_url[object_id]
-    next unless url
-
-    object_to_portfolios[object_id] ||= []
-    object_to_portfolios[object_id] << portfolio_id
-    portfolio = modify_portfolio(portfolio: portfolio, url: url)
+    url = portfolio_to_url[portfolio_id]
+    original_portfolio = get_response[:body]
+    portfolio = modify_portfolio(portfolio: original_portfolio, url: url)
     response = ApiUpdatePortfolio.new(mms_id: mms_id,
                                       portfolio_id: portfolio_id,
                                       api_key: api_key,
@@ -93,30 +77,20 @@ end
 ### Step 3: Write out report
 ### When I check if there's a mapping, report out when there isn't one
 ### For each portfolio, put the status of each call
-File.open("#{output_dir}/objects_updated.tsv", 'w') do |output|
-  output.puts("Object ID\tNew URL\tMMS ID\tPortfolio ID\tOld URL\tRetrieval Response\tUpdate Response")
-  object_to_url.each do |object_id, new_url|
-    portfolios = object_to_portfolios[object_id]
-    if portfolios
-      portfolios.each do |portfolio_id|
-        mms_id = bib_portfolios.select { |_id, portfolios| portfolios.include?(portfolio_id) }.first[0]
-        old_url = portfolio_to_old_url[portfolio_id]
-        retrieval = get_responses[portfolio_id]
-        retrieval_status = retrieval ? retrieval[:status] : nil
-        update = update_responses[portfolio_id]
-        update_status = update ? update[:status] : nil
-        output.write("#{object_id}\t")
-        output.write("#{new_url}\t")
-        output.write("#{mms_id}\t")
-        output.write("#{portfolio_id}\t")
-        output.write("#{old_url}\t")
-        output.write("#{retrieval_status}\t")
-        output.puts(update_status)
-      end
-    else
-      output.write("#{object_id}\t")
-      output.write("#{new_url}\t")
-      output.puts("\t\t\t\t")
+File.open("#{output_dir}/objects_updated_10-23-25.tsv", 'w') do |output|
+  output.puts("New URL\tMMS ID\tPortfolio ID\tRetrieval Response\tUpdate Response")
+  bib_portfolios.each do |mms_id, portfolios|
+    portfolios.each do |portfolio_id|
+      portfolio_url = portfolio_to_url[portfolio_id]
+      retrieval = get_responses[portfolio_id]
+      retrieval_status = retrieval ? retrieval[:status] : nil
+      update = update_responses[portfolio_id]
+      update_status = update ? update[:status] : nil
+      output.write("#{portfolio_url}\t")
+      output.write("#{mms_id}\t")
+      output.write("#{portfolio_id}\t")
+      output.write("#{retrieval_status}\t")
+      output.puts(update_status)
     end
   end
 end
