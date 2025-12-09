020ef9a0443469232e48bbb43aac4cab78cf51fd
Mark Zelesky <mzelesky@princeton.edu>
2025-12-09 11:16:07 -0500

    task to find filtered zhonghua records

--- tasks/find_zhonghua_records.rb
+++ tasks/find_zhonghua_records.rb
@@ -9,46 +9,55 @@ def tracking_field(isbn)
   field
 end
 
+def invalid_record?(record)
+  %w[3 u z].include?(record.leader[17]) ||
+    record['040']['b'] != 'eng' ||
+    record['245']['a'] !~ /[A-Za-z]/ ||
+    record['880'].nil? ||
+    record['948']['h'] =~ / 0 OTHER HOLDINGS/ ||
+    record.fields('600'..'655').empty?
+end
+
+def filtered_matches(identifier:, identifier_type:, conn:)
+  OCLCRecordMatch.new(identifier: identifier, identifier_type: identifier_type, conn: conn)
+                 .records.reject { |record| invalid_record?(record) }
+end
+
 ### If the URL of the record matches the URL of the spreadsheet, trust that record
 
 input_dir = ENV['DATA_INPUT_DIR']
 output_dir = ENV['DATA_OUTPUT_DIR']
 date = Time.new.strftime('%Y-%m-%d')
-processed = []
-writer = MARC::XMLWriter.new("#{output_dir}/all_zhonghua_records_with_isbns_#{date}.marcxml")
+processed = 0
+writer = MARC::XMLWriter.new("#{output_dir}/zhonghua_records_filtered_#{date}.marcxml")
 csv = CSV.open("#{input_dir}/zhonghua_list.csv", 'r', headers: true)
 conn = nil
 csv.each do |row|
   next if row['Existing MMS ID'][0] == '9'
 
-  urls = []
-  urls << row['URL'].gsub(%r{^https?://(.*)$}, '\1')
+  urls = [row['URL'].gsub(%r{^https?://(.*)$}, '\1')]
   urls << row['唯一标识符'] if row['唯一标识符'] =~ /^ZHB/
   matches = []
   tracking_id = nil
   urls.each do |url|
     next unless matches.empty?
 
-    if (processed.size % 1_000).zero? || conn.nil?
-      conn = Z3950Connection.new(host: OCLC_Z3950_ENDPOINT,
-                                 database_name: OCLC_Z3950_DATABASE_NAME,
+    if (processed % 1_000).zero? || conn.nil?
+      conn = Z3950Connection.new(host: OCLC_Z3950_ENDPOINT, database_name: OCLC_Z3950_DATABASE_NAME,
                                  credentials: { user: OCLC_Z3950_USER, password: OCLC_Z3950_PASSWORD })
     end
-    match_class = OCLCRecordMatch.new(identifier: url, identifier_type: 'url', conn: conn)
-    matches = match_class.records
+    matches = filtered_matches(identifer: url, identifier_type: 'url', conn: conn)
     tracking_id = url
-    processed << url
+    processed += 1
   end
   next unless matches.empty?
 
-  isbn = row['标准编号']
-  next unless isbn =~ /ISBN/
+  tracking_id = row['标准编号'] # Standard Number
+  next unless tracking_id =~ /ISBN/
 
-  normalized_isbn = isbn_normalize(isbn.gsub(/^ISBN (.*)$/, '\1'))
-  match_class = OCLCRecordMatch.new(identifier: normalized_isbn, identifier_type: 'isbn', conn: conn)
-  matches = match_class.records
-  tracking_id = isbn
-  processed << isbn
+  normalized_isbn = isbn_normalize(tracking_id.gsub(/^ISBN (.*)$/, '\1'))
+  matches = filtered_matches(identifier: normalized_isbn, identifier_type: 'isbn', conn: conn)
+  processed += 1
   matches.each do |record|
     record.append(tracking_field(tracking_id))
     writer.write(record)
