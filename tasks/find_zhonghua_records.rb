5476d6d83344b10558a2cd1ca93bb99d2a353ced
Mark Zelesky <mzelesky@princeton.edu>
2026-01-15 15:12:23 -0500

    update zhonghua record script
    Co-authored-by: Tom Ventimiglia <tventimi@users.noreply.github.com>

--- tasks/find_zhonghua_records.rb
+++ tasks/find_zhonghua_records.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative './../lib/lsp-data'
+require_relative '../lib/lsp-data'
 require 'csv'
 
 def tracking_field(isbn)
@@ -9,26 +9,76 @@ def tracking_field(isbn)
   field
 end
 
+def insufficient_subjects?(record)
+  record['050'].nil? || record.fields('600'..'655').empty?
+end
+
 def invalid_record?(record)
   %w[3 u z].include?(record.leader[17]) ||
     record['040']['b'] != 'eng' ||
     record['245']['a'] !~ /[A-Za-z]/ ||
     record['880'].nil? ||
     record['948']['h'] =~ / 0 OTHER HOLDINGS/ ||
-    record.fields('600'..'655').empty?
+    insufficient_subjects?(record)
 end
 
-def filtered_matches(identifier:, identifier_type:, conn:)
+def title_type(row)
+  if row['其他题名信息']
+    'collection'
+  else
+    'single'
+  end
+end
+
+def match_title(title_type:, row:)
+  if title_type == 'single'
+    row['题名']
+  else
+    row['其他题名信息'].gsub(/^\*(.*)$/, '\1')
+  end
+end
+
+def matched_title?(row:, record:)
+  title_to_match = match_title(title_type: title_type(row), row: row)
+  f880 = record.fields('880').find { |field| field['6'][0..2] == '245' }
+  f880.nil? || /^#{title_to_match}/.match?(f880['a'])
+end
+
+def series_requirement?(row:, record:)
+  if row['丛编']
+    record.fields('490').size.positive?
+  else
+    true
+  end
+end
+
+def filtered_matches(identifier:, identifier_type:, conn:, row:)
   OCLCRecordMatch.new(identifier: identifier, identifier_type: identifier_type, conn: conn)
-                 .records.reject { |record| invalid_record?(record) }
+                 .records.reject do |record|
+                   invalid_record?(record) &&
+                     !matched_title?(row: row, record: record) &&
+                     !series_requirement?(row: row, record: record)
+                 end
+end
+
+def add_isbn_matches(conn:, row:)
+  hash = {}
+  hash[:tracking_id] = row['标准编号'] # Standard Number
+  hash[:matches] = []
+  return hash unless hash[:tracking_id] =~ /ISBN/
+
+  normalized_isbn = isbn_normalize(hash[:tracking_id].gsub(/^ISBN (.*)$/, '\1'))
+  hash[:matches] += filtered_matches(identifier: normalized_isbn, identifier_type: 'isbn', conn: conn, row: row)
+  hash
 end
 
 ### If the URL of the record matches the URL of the spreadsheet, trust that record
 
-input_dir = ENV['DATA_INPUT_DIR']
-output_dir = ENV['DATA_OUTPUT_DIR']
+input_dir = ENV.fetch('DATA_INPUT_DIR', nil)
+output_dir = ENV.fetch('DATA_OUTPUT_DIR', nil)
 date = Time.new.strftime('%Y-%m-%d')
 processed = 0
+single_writer = MARC::XMLWriter.new("#{output_dir}/zhonghua_records_filtered_one_per_match_#{date}.marcxml")
 writer = MARC::XMLWriter.new("#{output_dir}/zhonghua_records_filtered_#{date}.marcxml")
 csv = CSV.open("#{input_dir}/zhonghua_list.csv", 'r', headers: true)
 conn = nil
@@ -37,6 +87,7 @@ csv.each do |row|
 
   urls = [row['URL'].gsub(%r{^https?://(.*)$}, '\1')]
   urls << row['唯一标识符'] if row['唯一标识符'] =~ /^ZHB/
+  urls << row['著录号'] if row['著录号'] =~ /^ZSK/
   matches = []
   tracking_id = nil
   urls.each do |url|
@@ -46,21 +97,21 @@ csv.each do |row|
       conn = Z3950Connection.new(host: OCLC_Z3950_ENDPOINT, database_name: OCLC_Z3950_DATABASE_NAME,
                                  credentials: { user: OCLC_Z3950_USER, password: OCLC_Z3950_PASSWORD })
     end
-    matches = filtered_matches(identifer: url, identifier_type: 'url', conn: conn)
+    matches = filtered_matches(identifier: url, identifier_type: 'url', conn: conn, row: row)
     tracking_id = url
     processed += 1
   end
-  next unless matches.empty?
-
-  tracking_id = row['标准编号'] # Standard Number
-  next unless tracking_id =~ /ISBN/
-
-  normalized_isbn = isbn_normalize(tracking_id.gsub(/^ISBN (.*)$/, '\1'))
-  matches = filtered_matches(identifier: normalized_isbn, identifier_type: 'isbn', conn: conn)
+  if matches.empty?
+    isbn_matches = add_isbn_matches(conn: conn, row: row)
+    matches += isbn_matches[:matches]
+    tracking_id = isbn_matches[:tracking_id]
+  end
   processed += 1
-  matches.each do |record|
+  matches.each_with_index do |record, index|
     record.append(tracking_field(tracking_id))
     writer.write(record)
+    single_writer.write(record) if index.zero?
   end
 end
 writer.close
+single_writer.close
