d451875292040fb1458e3291be00be78bee2c694
Thomas Ventimiglia <tventimi@princeton.edu>
2026-03-12 10:12:10 -0400

    Represent SIP chars in 8-digit format

--- spec/chinese_conversion_spec.rb
+++ spec/chinese_conversion_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe LspData::ChineseConversion do
     described_class.new(chi_simp_title)
   end
 
-  let(:chi_simp_title) { '中华人民共和国' }
+  let(:chi_simp_title) { '中华人民共和国~㛟𠰷' }
 
   context 'Chinese title with some Simplified characters and characters that are the same in both systems' do
     it 'returns Simplified characters converted to Traditional, with all other characters unchanged' do
-      expect(chinese_conv.converted).to eq '中華人民共和國'
+      expect(chinese_conv.converted).to eq '中華人民共和國~𡞵嚧'
     end
   end
 end
