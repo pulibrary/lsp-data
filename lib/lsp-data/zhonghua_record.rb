16647f66315ca46a273ca56858c8db1257d61ed6
tventimi <tventimi@princeton.edu>
2026-04-09 10:50:38 -0400

    Create zhonghua_record class and spec (#186)
    * Create zhonghua_record class and spec
    
    * title_type tests for leading asterisk
    
    * add collection to spec
    
    * add comma
    
    * Refactoring ZhonghuaRecord class
    
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>
    
    * use title_info structure, field-specific replacement methods
    
    * fix 880$6 rspec test
    
    * remove new_field method from zhonghua_record
    
    * document field-specific replacement methods
    
    * fix trailing space
    
    * make internal methods private for zhonghua_record
    
    * Handle case where no WC record is found
    
    * handle nil value for searching zhonghua
    
    ---------
    
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

---
+++ lib/lsp-data/zhonghua_record.rb
@@ -0,0 +1,132 @@
+# frozen_string_literal: true
+
+### Codebase for parsing LSP data
+module LspData
+  ### Given a set of spreadsheet rows for the 'Zhonghua jing dian gu ji ku' database,
+  ###   retrieve a matching record from OCLC and apply a set of transformations.
+  ### Required elements:
+  ###   1. From the Zhonghua CSV file:
+  ###     a. Main Title
+  ###     b. Series Title (may not have a series title)
+  ###     c. URL
+  ###     d. Alternate url
+  ###     e. ISBN
+  ###   2. A Z3950connection object (already connected)
+  class ZhonghuaRecord
+    attr_reader :original_record, :main_title, :series_title, :url, :alternate_url, :isbn, :conn
+
+    def initialize(title_info:, conn:)
+      @conn = conn
+      @main_title = title_info[:main_title]
+      @series_title = title_info[:series_title]
+      @url = title_info[:url]
+      @alternate_url = title_info[:alternate_url]
+      @isbn = title_info[:isbn]
+      @original_record ||= retrieve_record
+    end
+
+    ### Applies field additions/replacements/deletions using data in the row(s)
+    def transformed_record
+      return if original_record.nil?
+
+      new_record = MarcCleanup.duplicate_record(original_record)
+      delete_fields(record: new_record, tag_list: %w[001 003 035 019 029 505 856])
+      replace_title_field(record: new_record, main_title: main_title)
+      replace_series_fields(record: new_record, series_title: series_title)
+      replace_url(record: new_record, url: url)
+      new_record
+    end
+
+    private
+
+    ### Retrieves a WorldCat record using the search data in the first row
+    def retrieve_record
+      search_keys.reject { |key| key[:id].nil? }.each do |key|
+        record = filtered_match(identifier: key[:id], identifier_type: key[:type])
+        return record unless record.nil?
+      end
+      nil
+    end
+
+    ### Creates an array of search keys in the order to be attempted
+    def search_keys
+      [
+        { id: url, type: 'url' },
+        { id: alternate_url, type: 'url' },
+        { id: isbn, type: 'isbn' }
+      ]
+    end
+
+    ### Deletes all fields with the tags in the list, along with their associated 880s
+    def delete_fields(record:, tag_list:)
+      record.fields.delete_if do |field|
+        tag_list.include?(field.tag) ||
+          (field.tag == '880' && tag_list.include?(field['6'][0..2]))
+      end
+    end
+
+    ### Deletes all existing instances of a field, and then adds a new instance
+    def replace_field(record:, field:)
+      delete_fields(record: record, tag_list: [field.tag])
+      record.append(field)
+    end
+
+    ### Replaces the title (245a) field
+    def replace_title_field(record:, main_title:)
+      replace_field(record: record, field: MARC::DataField.new('245', '0', '0', MARC::Subfield.new('a', main_title)))
+    end
+
+    ### Replaces the series (490a and 830a) fields
+    def replace_series_fields(record:, series_title:)
+      replace_field(record: record, field: MARC::DataField.new('490', '1', ' ', MARC::Subfield.new('a', series_title)))
+      replace_field(record: record, field: MARC::DataField.new('830', ' ', '0', MARC::Subfield.new('a', series_title)))
+    end
+
+    ### Replaces the URL (956u) field
+    def replace_url(record:, url:)
+      replace_field(record: record, field: MARC::DataField.new('956', '4', '1', MARC::Subfield.new('u', "https://#{url}")))
+    end
+
+    ### Creates a field 917 containing the given identifier
+    def tracking_field(identifier)
+      MARC::DataField.new('917', ' ', ' ', MARC::Subfield.new('a', identifier))
+    end
+
+    ### Performs a Z39.50 search with the given identifier, filters results
+    ###   Based on crieteria in the given row, returns the first result (if any)
+    def filtered_match(identifier:, identifier_type:)
+      match = OCLCRecordMatch.new(identifier: identifier, identifier_type: identifier_type, conn: conn)
+                             .records.find do |record|
+        !invalid_record?(record) && matched_title?(record)
+      end
+      return unless match
+
+      match.append(tracking_field(identifier))
+      match
+    end
+  end
+
+  ### Indicates if a record is of suffient quality to be used
+  def invalid_record?(record)
+    %w[3 u z].include?(record.leader[17]) ||
+      record['040']['b'] != 'eng' ||
+      record['245']['a'] !~ /[A-Za-z]/ ||
+      record['880'].nil? ||
+      record['948']['h'] =~ / 0 OTHER HOLDINGS/ ||
+      insufficient_subjects?(record)
+  end
+
+  ### Indicates if a record lacks enough classification data
+  def insufficient_subjects?(record)
+    record['050'].nil? || record.fields('600'..'655').empty?
+  end
+end
+
+### Indicates if the title in the row matches the title in the record
+def matched_title?(record)
+  f880 = record.fields('880').find { |field| field['6'][0..2] == '245' }
+  return false unless f880
+
+  f880a_trad = ChineseConversion.new(f880['a']).converted
+  /^#{main_title}/.match?(f880a_trad)
+end
---
+++ spec/zhonghua_record_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative '../lib/lsp-data'
+require 'spec_helper'
+
+RSpec.describe LspData::ZhonghuaRecord do
+  subject(:zhonghua_record) do
+    described_class.new(title_info: { main_title: main_title, series_title: series_title,
+                                      url: url, alternate_url: alternate_url,
+                                      isbn: isbn }, conn: conn)
+  end
+
+  let(:conn) do
+    Z3950Connection.new(host: OCLC_Z3950_ENDPOINT, database_name: OCLC_Z3950_DATABASE_NAME,
+                        credentials: { user: OCLC_Z3950_USER, password: OCLC_Z3950_PASSWORD })
+  end
+
+  context 'no WorldCat record is found' do
+    let('main_title') { 'no title' }
+    let('series_title') { 'series' }
+    let('url') { 'thisisnotaurl' }
+    let('alternate_url') { nil }
+    let('isbn') { nil }
+
+    it 'returns nil' do
+      expect(zhonghua_record.original_record).to be_nil
+      expect(zhonghua_record.transformed_record).to be_nil
+    end
+  end
+
+  context 'single row where ISBN is found in WorldCat, but URLs are not' do
+    let('main_title') { '龍文鞭影' }
+    let('series_title') { 'CHINESE_SERIES' }
+    let('url') { 'jingdian.ancientbooks.cn/6/book/4855843' }
+    let('alternate_url') { 'ZHB011009379' }
+    let('isbn') { '9787101146899' }
+
+    it 'returns a record containing the ISBN, other fields transformed according to row data' do
+      new_record = zhonghua_record.transformed_record
+      expect(new_record['001']).to be nil
+      expect(new_record.fields('020').map(&:value)).to include isbn
+      expect(new_record['245'].value).to eq main_title
+      expect(new_record.fields('880').map { |f| f['6'].to_s[0..2] }).not_to include(%w[245 490 505 830])
+      expect(new_record['490'].value).to eq series_title
+      expect(new_record['830'].value).to eq series_title
+      expect(new_record['956'].value).to eq "https://#{url}"
+    end
+  end
+end
